1d592b06065464840b34c3e2a57519b14fa79b50
Mia Darvasi <miadarv@gmail.com>
2025-05-06 10:26:23 +0300

    build for prod

--- server.js
+++ server.js
@@ -46,6 +46,15 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.resolve(__dirname, 'frontend', 'dist')));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+//     });
+// }
+
 if (process.env.NODE_ENV !== 'production') {
     app.get('*', (req, res) => {
         res.status(404).send('API route not found')
