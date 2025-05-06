ee554048c6cc672630dffed216f6f28761b5430a
Mia Darvasi <miadarv@gmail.com>
2025-05-06 14:14:49 +0300

    fix db connection

--- config/prod.js
+++ config/prod.js
@@ -1,23 +1,20 @@
-// export default {
-//   dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-//   dbName : process.env.DB_NAME || 'JaMoveo'
-// }
 
 
 // export default {
-//   dbURL: process.env.MONGO_URL || 'mongodb+srv://miadarv:GqMPXDXdPLZzzBhu@cluster0.dxjxfxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-//   dbName: process.env.DB_NAME || 'JaMoveo'
-// }
-
-// export default {
-//   dbURL: process.env.MONGO_URL || 'mongodb+srv://miadarv:batcheN2@cluster0.dxjxfxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+//   dbURL: process.env.MONGO_URL || 'mongodb+srv://miadarv:batcheN2>@cluster0.dxjxfxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 //   dbName: process.env.DB_NAME || 'JaMoveo'
 // }
 
 
+
+// export default {
+//   dbURL: process.env.MONGO_URL || 'mongodb+srv://dbuser:dbuser>@cluster0.dxjxfxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+//   dbName: process.env.DB_NAME || 'JaMoveo'
+// }
 
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://miadarv:batcheN2>@cluster0.dxjxfxs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://miadarv:eRvxGeAUTXqWUnSz@jamoveo.n4bggmu.mongodb.net/?retryWrites=true&w=majority&appName=JaMoveo',
   dbName: process.env.DB_NAME || 'JaMoveo'
 }
+
 
--- server.js
+++ server.js
@@ -46,15 +46,6 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'frontend', 'dist')));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-//     });
-// }
-
 if (process.env.NODE_ENV !== 'production') {
     app.get('*', (req, res) => {
         res.status(404).send('API route not found')
